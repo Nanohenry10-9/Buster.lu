1785df765cd7034b8c53c5684d1f9659d81d27ca
Nanohenry10-9 <irneha@hotmail.com>
2018-03-16 22:12:23 +0100

    More functionality
    So much WOW....
    (no really it almost works)

--- backbone.js
+++ backbone.js
@@ -13,8 +13,10 @@ $(document).ready(function() {
 				var row = document.createElement("tr");
 				var c1 = document.createElement("th");
 				var c2 = document.createElement("th");
-				c1.innerHTML = json[i]["properties"]["name"];
-				c2.innerHTML = json[i]["properties"]["id"];
+				var c3 = document.createElement("a");
+				a.innerHTML = json[i]["properties"]["name"];
+				a.onclick = "openPopup(" + json[i]["properties"]["id"] + ")";
+				c1.appendChild(a);
 				row.appendChild(c1);
 				row.appendChild(c2);
 				list.appendChild(row);
@@ -23,7 +25,34 @@ $(document).ready(function() {
 	}
 
 	function openPopup(id) {
-
+		//var box = document.getElementById("pop-up-div");
+		//box.visibility = "visible";
+		var textbox = document.getElementById("pop-up-text");
+		textbox.innerHTML = "Bus stop number: " + id;
+		var list = document.getElementById("table-content-popup");
+		$("#table-content-popup > tr").slice(0).remove();
+		var t1 = document.createElement("tr");
+		var t2 = document.createElement("th");
+		t2.innerHTML = "Please wait...";
+		t1.appendChild(t2);
+		list.appendChild(t1);
+		var req = new XMLHttpRequest();
+		var url = "https://api.tfl.lu/v1/StopPoint/Departures/" + id;
+		req.send();
+		req.onload = function() {
+			var json = JSON.parse(req.response);
+			$("#table-content-popup > tr").slice(0).remove();
+			for (var i = 0; i < json.length && i < 5; i++) {
+				var row = document.createElement("tr");
+				var c1 = document.createElement("th");
+				var c2 = document.createElement("th");
+				c1.innerHTML = json[i]["number"];
+				c2.innerHTML = json[i]["departure"];
+				row.appendChild(c1);
+				row.appendChild(c2);
+				list.appendChild(row);
+			}
+		}
 	}
 
 	$("#input-text").keyup(function(event) {
