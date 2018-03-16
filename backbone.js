451bb19b6105c1f80872595a276ca88f2714fc30
Nanohenry10-9 <irneha@hotmail.com>
2018-03-16 22:52:29 +0100

    Added much wow
    It actually works now

--- backbone.js
+++ backbone.js
@@ -9,14 +9,18 @@ $(document).ready(function() {
 			var json = JSON.parse(req.response).features;
 			var list = document.getElementById("table-content");
 			$("#table-content > tr").slice(0).remove();
-			for (var i = 0; i < json.length; i++) {
+			for (var i = 0; i < json.length && i < 5; i++) {
 				var row = document.createElement("tr");
 				var c1 = document.createElement("th");
 				var c2 = document.createElement("th");
 				var c3 = document.createElement("a");
-				a.innerHTML = json[i]["properties"]["name"];
-				a.onclick = "openPopup(" + json[i]["properties"]["id"] + ")";
-				c1.appendChild(a);
+				c3.innerHTML = json[i]["properties"]["name"];
+				c3.setAttribute("data-theme", json[i]["properties"]["id"]);
+				c3.addEventListener("click", function(event) {
+					openPopup(c3.getAttribute("data-theme"));
+					event.preventDefault();
+				});
+				c1.appendChild(c3);
 				row.appendChild(c1);
 				row.appendChild(c2);
 				list.appendChild(row);
@@ -26,7 +30,7 @@ $(document).ready(function() {
 
 	function openPopup(id) {
 		//var box = document.getElementById("pop-up-div");
-		//box.visibility = "visible";
+		//box[visibility] = "visible";
 		var textbox = document.getElementById("pop-up-text");
 		textbox.innerHTML = "Bus stop number: " + id;
 		var list = document.getElementById("table-content-popup");
@@ -38,6 +42,7 @@ $(document).ready(function() {
 		list.appendChild(t1);
 		var req = new XMLHttpRequest();
 		var url = "https://api.tfl.lu/v1/StopPoint/Departures/" + id;
+		req.open("GET", url);
 		req.send();
 		req.onload = function() {
 			var json = JSON.parse(req.response);
@@ -46,8 +51,18 @@ $(document).ready(function() {
 				var row = document.createElement("tr");
 				var c1 = document.createElement("th");
 				var c2 = document.createElement("th");
-				c1.innerHTML = json[i]["number"];
-				c2.innerHTML = json[i]["departure"];
+				c1.innerHTML = json[i]["line"];
+				var date = new Date(Number(json[i]["departure"]) * 1000);
+				var hours = date.getHours();
+				var minutes = "0" + date.getMinutes();
+				if (minutes.length == 3) {
+					minutes = minutes.substr(1, 3);
+				}
+				c2.innerHTML = hours + ":" + minutes;
+				if (Number(json[i]["delay"]) >= 60) {
+					c2.style.color = "red";
+				}
+				c2.innerHTML = c2.innerHTML + ", " + Number(json[i]["delay"]) / 60;
 				row.appendChild(c1);
 				row.appendChild(c2);
 				list.appendChild(row);
