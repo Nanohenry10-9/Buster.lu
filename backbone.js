05a3dee0d46c161d2569662eec3c0bad169cac28
Leo0a8est <leo0a8est@gmail.com>
2018-03-17 12:34:47 +0100

    Put the QR Code code in place (commented)

--- backbone.js
+++ backbone.js
@@ -8,6 +8,15 @@ function loaded() {
 
 $(document).ready(function() {
 
+	/*var qrcode = new QRCode("test", {
+    	text: "http://jindo.dev.naver.com/collie",
+    	width: 128,
+    	height: 128,
+    	colorDark : "#000000",
+    	colorLight : "#ffffff",
+    	correctLevel : QRCode.CorrectLevel.H
+	});*/
+
 	var display_interval;
 	var clock_interval;
 
