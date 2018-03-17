b311b0f949aca4c04ff56ae9dd50fbe409f17634
Nanohenry10-9 <irneha@hotmail.com>
2018-03-17 08:13:26 +0100

    Added real-time display
    Updates every 30 sec

--- backbone.js
+++ backbone.js
@@ -5,6 +5,8 @@ function loaded() {
 
 $(document).ready(function() {
 
+	var interval;
+
 	function search() {
 		var text = document.getElementById("input-text").value;
 		var req = new XMLHttpRequest();
@@ -42,7 +44,7 @@ $(document).ready(function() {
 		$("#all").addClass("view-hidden");
 		$("#pop-up-div").removeClass("view-hidden");
 		$("#pop-up-div").addClass("view-shown");
-		history.pushState(null, "Buster.lu - Bus stop " + name, "/" + id);
+		//history.pushState(null, "Buster.lu - Bus stop " + name, "/" + id);
 		console.log("Popup for: " + id + ", " + name);
 		var textbox = document.getElementById("pop-up-text");
 		textbox.innerHTML = "Bus stop: " + name;
@@ -53,39 +55,41 @@ $(document).ready(function() {
 		t2.innerHTML = "Please wait...";
 		t1.appendChild(t2);
 		list.appendChild(t1);
+		display(id);
+		interval = setInterval(display, 30000, id);
+	}
+
+	function display(id) {
 		var req = new XMLHttpRequest();
 		var url = "https://api.tfl.lu/v1/StopPoint/Departures/" + id;
 		req.open("GET", url);
 		req.send();
 		req.onload = function() {
 			var json = JSON.parse(req.response);
-			if (json.length != 0) {
-				$("#table-content-popup > tr").slice(0).remove();
-				for (var i = 0; i < json.length && i < 5; i++) {
-					var row = document.createElement("tr");
-					var c1 = document.createElement("th");
-					var c2 = document.createElement("th");
-					var c3 = document.createElement("th");
-					c1.innerHTML = json[i]["line"];
-					c2.innerHTML = json[i]["destination"];
-					var delay = json[i]["delay"] / 60;
-					var date = new Date((json[i]["departure"] + delay) * 1000);
-					var hours = date.getHours();
-					var minutes = "0" + date.getMinutes();
-					if (minutes.length == 3) {
-						minutes = minutes.substr(1, 3);
-					}
-					c3.innerHTML = hours + ":" + minutes;
-					if (delay > 0) {
-						c3.innerHTML = c3.innerHTML + " (" + delay + " min. late)";
-					}
-					row.appendChild(c1);
-					row.appendChild(c2);
-					row.appendChild(c3);
-					list.appendChild(row);
+			var list = document.getElementById("table-content-popup");
+			$("#table-content-popup > tr").slice(0).remove();
+			for (var i = 0; i < json.length && i < 5; i++) {
+				var row = document.createElement("tr");
+				var c1 = document.createElement("th");
+				var c2 = document.createElement("th");
+				var c3 = document.createElement("th");
+				c1.innerHTML = json[i]["line"];
+				c2.innerHTML = json[i]["destination"];
+				var delay = json[i]["delay"] / 60;
+				var date = new Date((json[i]["departure"] + delay) * 1000);
+				var hours = date.getHours();
+				var minutes = "0" + date.getMinutes();
+				if (minutes.length == 3) {
+					minutes = minutes.substr(1, 3);
 				}
-			} else {
-				t2.innerHTML = "Could not load data.";
+				c3.innerHTML = hours + ":" + minutes;
+				if (delay > 0) {
+					c3.innerHTML = c3.innerHTML + " (" + delay + " min. late)";
+				}
+				row.appendChild(c1);
+				row.appendChild(c2);
+				row.appendChild(c3);
+				list.appendChild(row);
 			}
 		}
 	}
